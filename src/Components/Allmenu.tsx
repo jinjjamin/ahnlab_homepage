4caa716c757c1b9eaf107b33e178caa3890e9e26
jinjjamin <jinjjamin@gmail.com>
2025-06-12 17:20:52 +0900

    test

--- src/Components/Allmenu.tsx
+++ src/Components/Allmenu.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 /* Components */
@@ -9,13 +9,27 @@ interface AllmenuProps {
 }
 
 const Allmenu:React.FC<AllmenuProps> = ({onClose}) => {
+    const [visible, setVisible] = useState(false);
+
+    // mount 후 transition 시작
+    useEffect(() => {
+        setTimeout(() => setVisible(true), 10);
+    }, []);
+
+    const handleClose = () => {
+        setVisible(false);
+        setTimeout(() => {
+            onClose(); // 부모 컴포넌트에서 isOpen false로
+        }, 300); // transition-duration에 맞춤
+    };
+
     return (
         <>
-            <div className="allMenuWrap">
+            <div className={`allMenuWrap ${visible ? 'show' : 'hide'}`}>
                 <div className="allMenu">
                     <div className="allMenuHead">
                         <div className="head">
-                            <Link to="#" className="btnAllMenuClose" onClick={onClose}>전체메뉴 닫기</Link>
+                            <Link to="#" className="btnAllMenuClose" onClick={handleClose}>전체메뉴 닫기</Link>
                         </div>
                         <div className="cont">
                             <SearchTextField type={'text'} placeholder={'검색어 입력'} />
